4e24c67123e5d7064eaf95768e4a4fe5076ae159
david yourdon <dyourdon@dyourdon-ltm1.internal.salesforce.com>
2019-08-23 11:09:34 -0400

    newline

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
