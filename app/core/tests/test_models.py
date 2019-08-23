c97418dea23ca863f660c9aa6b25d03aede94db7
david yourdon <dyourdon@dyourdon-ltm1.internal.salesforce.com>
2019-08-23 11:17:24 -0400

    Require valid email test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,8 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
