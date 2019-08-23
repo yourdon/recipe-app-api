472d1c930df58aaaba3fe65cd9c9c33b8a1ec253
david yourdon <dyourdon@dyourdon-ltm1.internal.salesforce.com>
2019-08-23 11:09:05 -0400

    blerg

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,14 +4,14 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-	def test_create_user_with_email_successful(self):
-		"""Test creating a new user with an email is succesful"""
-		email = 'test@me.com'
-		password = 'abc123'
-		user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is succesful"""
+        email = 'test@me.com'
+        password = 'abc123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
-		self.assertEqual(user.email, email)
-		self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
